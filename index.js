31c47d7e53459f2320c7b3dd99f6bdcd0a4556a0
Braulio Mora <brauliocdc59@gmail.com>
2021-11-26 16:01:19 -0800

    initialized project libraries and dependencies

---
+++ index.js
@@ -0,0 +1,8 @@
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Questions = require("./lib/Questions");
+const {promp} = require("inquirer");
+const {writeFile} = require("fs");
+
